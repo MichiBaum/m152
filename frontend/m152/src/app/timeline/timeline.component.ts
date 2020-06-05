987f716ae732d7ba2ba8eebb1203a87475321b0c
MichiBaum <michael_baumberger@gmx.ch>
2020-06-05 16:28:15 +0200

    update

--- frontend/m152/src/app/timeline/timeline.component.ts
+++ frontend/m152/src/app/timeline/timeline.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
 
 @Component({
   selector: 'app-timeline',
@@ -18,26 +17,37 @@ export class TimelineComponent implements OnInit {
 
   ngOnInit(): void {
     this.options = {
-      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+      plugins: [dayGridPlugin, timeGridPlugin],
       defaultDate: '2020-06-05',
+      agenda: 'h:mm{ - h:mm}',
       header: {
-        left: 'prev,next',
+        left: 'prev,next today',
         center: 'title',
-        right: 'month,agendaWeek,agendaDay'
+        right: 'dayGridMonth,dayGridWeek,timeGridDay'
       },
       height: window.innerHeight / 100 * 80
     };
 
     this.events = [
       {
+        title: 'Input Copyright',
+        start: '2020-05-29T13:00:00',
+        end: '2020-05-29T14:30:00'
+      },
+      {
+        title: 'Input SVG',
+        start: '2020-06-05T13:00:00',
+        end: '2020-06-05T14:05:00'
+      },
+      {
         title: 'Webseite Grundgerüst',
-        start: '2020-06-05',
-        end: '2020-06-05'
+        start: '2020-06-05T14:10:00',
+        end: '2020-06-05T15:40:00'
       },
       {
         title: 'Kalender für Zeitlinie einbauen',
-        start: '2020-06-05',
-        end: '2020-06-05'
+        start: '2020-06-05T15:40:00',
+        end: '2020-06-05T16:20:00'
       }
     ];
   }
