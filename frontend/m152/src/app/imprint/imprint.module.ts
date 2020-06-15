61b0c102eb650041a454fcbaf73e8425f4972b47
MichiBaum <michael_baumberger@gmx.ch>
2020-06-15 11:27:08 +0200

    Added imprint module and component but no text
    - Added imprint module and component but no text

---
+++ frontend/m152/src/app/imprint/imprint.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ImprintComponent } from './imprint.component';
+
+
+
+@NgModule({
+  declarations: [ImprintComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ImprintModule { }
--- frontend/m152/src/app/navigation/navigation.component.ts
+++ frontend/m152/src/app/navigation/navigation.component.ts
@@ -45,7 +45,14 @@ export class NavigationComponent implements OnInit {
         command: () => {
           this.sidebarVisible = false;
         }
-      } as MenuItem,
+      } as MenuItem,{
+        label: this.translate.instant('navigation.imprint'),
+        icon: 'pi pi-info',
+        routerLink: 'imprint',
+        command: () => {
+          this.sidebarVisible = false;
+        }
+      } as MenuItem
     ] as MenuItem[];
   }
 
