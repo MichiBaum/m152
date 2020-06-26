5ca4918af2dc575d56ed682e01f2b727193bd10b
MichiBaum <michael_baumberger@gmx.ch>
2020-06-26 14:31:42 +0200

    Added Header Animation

--- frontend/m152/src/app/header/header.component.ts
+++ frontend/m152/src/app/header/header.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import {PrimeNgBase} from '../core/models/primeng-base.model';
 import {LanguageConfig} from '../core/language.config';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('slideLeftMiddleRight', [
+      state('left', style({transform: 'translateX(-30%) rotate(-10deg)'})),
+      state('middle', style({transform: 'translateX(0%) rotate(0deg)'})),
+      state('right', style({transform: 'translateX(30%) rotate(10deg)'})),
+      transition('left => middle', animate('500ms ease-in')),
+      transition('middle => right', animate('500ms ease-in')),
+      transition('right => left', animate('500ms ease-in'))
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
 
   languages: PrimeNgBase[];
   selectedLanguage: PrimeNgBase;
+  headerAnimationState = 'middle';
 
   constructor(public languageConfig: LanguageConfig) { }
 
@@ -33,5 +45,15 @@ export class HeaderComponent implements OnInit {
 
   private initSelectedLanguage = (): PrimeNgBase => {
     return {field: this.languageConfig.current.isoCode, label: this.languageConfig.current.name} as PrimeNgBase;
+  }
+
+  toggleHeaderAnimationState() {
+    if (this.headerAnimationState === 'left') {
+      this.headerAnimationState = 'middle';
+    } else if (this.headerAnimationState === 'middle') {
+      this.headerAnimationState = 'right';
+    } else if (this.headerAnimationState === 'right') {
+      this.headerAnimationState = 'left';
+    }
   }
 }
