c9d70d6d25c36106b5b6a7b469b142bf3d191567
MichiBaum <michael_baumberger@gmx.ch>
2020-07-05 14:00:15 +0200

    Updated concept

--- frontend/m152/src/app/concept/concept.module.ts
+++ frontend/m152/src/app/concept/concept.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConceptComponent } from './concept.component';
+import {CardModule} from 'primeng';
+import {TranslateModule} from '@ngx-translate/core';
+import {AnimateModule} from '../animate/animate.module';
 
 
 
 @NgModule({
   declarations: [ConceptComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CardModule,
+    TranslateModule,
+    AnimateModule
   ]
 })
 export class ConceptModule { }
