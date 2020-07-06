f6b0ea1228783b782fb4954ece8648c64e8df2f0
MichiBaum <michael_baumberger@gmx.ch>
2020-07-06 08:23:00 +0200

    chnaged to manifest.webmanifest & added sw updater

--- frontend/m152/src/app/app.component.ts
+++ frontend/m152/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {SwUpdate} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private swupdate: SwUpdate) {
+    // checks if update available
+    this.swupdate.available.subscribe((event: any) => {
+      // reload / refresh the browser
+      this.swupdate.activateUpdate().then(() => document.location.reload());
+      console.log('sw updated');
+    });
+  }
+
 }
